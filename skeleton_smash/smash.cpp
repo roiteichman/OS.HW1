47250d59b6698be686fdac6684d8ac9a3a7d511b
teich <roi.teichman@campus.technion.ac.il>
2023-04-24 17:31:31 +0300

    main

--- skeleton_smash/smash.cpp
+++ skeleton_smash/smash.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
     }
 */
     //TODO: setup sig alarm handler
-
+/*
     Job tmp_1(1, 657, BACKGROUND, "cd C:\\Users\\teich");
     Job tmp_2(2, 658, FOREGROUND, "cd C:\\Users");
     Job tmp_3(3, 659, STOPPED, "ls");
@@ -29,11 +29,11 @@ int main(int argc, char* argv[]) {
 
 
 
-    for (int i = 0; i < 900000000; ++i) {}
-    std::cout << tmp_1 << std::endl;
+    //for (int i = 0; i < 900000000; ++i) {}
+    //std::cout << tmp_1 << std::endl;
 //    Job(int job_id, int pid, STATE state, char* cmd_line);
+*/
 
-/*
     SmallShell& smash = SmallShell::getInstance();
 
 
@@ -43,7 +43,7 @@ int main(int argc, char* argv[]) {
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
     }
-
+/*
     const char* cmd_line = "cd C:\\Users\\teich\\";
     char* last_path = "C:\\Users";
 
