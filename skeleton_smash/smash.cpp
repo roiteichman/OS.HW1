1966a99bfc6e880386fb76c7229aec7ec2b63801
teich <roi.teichman@campus.technion.ac.il>
2023-04-24 17:34:32 +0300

    print job list test

--- skeleton_smash/smash.cpp
+++ skeleton_smash/smash.cpp
@@ -17,22 +17,28 @@ int main(int argc, char* argv[]) {
     }
 */
     //TODO: setup sig alarm handler
-/*
+
     Job tmp_1(1, 657, BACKGROUND, "cd C:\\Users\\teich");
     Job tmp_2(2, 658, FOREGROUND, "cd C:\\Users");
     Job tmp_3(3, 659, STOPPED, "ls");
     Job tmp_4(4, 660, BACKGROUND, "pwd");
 
     JobsList jobs_list;
-    //jobs_list.addNewJob()
+    jobs_list.addNewJob(&tmp_1);
+    jobs_list.addNewJob(&tmp_2);
+    jobs_list.addNewJob(&tmp_3);
+    jobs_list.addNewJob(&tmp_4);
+
+    jobs_list.printJobsList();
+
+
+   // for (int i = 0; i < 900000000; ++i) {}
+   // std::cout << tmp_1 << std::endl;
 
 
 
 
-    //for (int i = 0; i < 900000000; ++i) {}
-    //std::cout << tmp_1 << std::endl;
-//    Job(int job_id, int pid, STATE state, char* cmd_line);
-*/
+/*
 
     SmallShell& smash = SmallShell::getInstance();
 
@@ -43,7 +49,12 @@ int main(int argc, char* argv[]) {
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
     }
-/*
+
+
+
+
+
+
     const char* cmd_line = "cd C:\\Users\\teich\\";
     char* last_path = "C:\\Users";
 
