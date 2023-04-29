db8b8e9f15ca22521e1bf30dde024602a5aec6c7
teich <roi.teichman@campus.technion.ac.il>
2023-04-29 21:02:21 +0300

    jobs (main)

--- skeleton_smash/smash.cpp
+++ skeleton_smash/smash.cpp
@@ -46,7 +46,6 @@ int main(int argc, char* argv[]) {
         std::string cmd_line;
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
-        smash.getMJobList().printJobsList();
     }
 
 
