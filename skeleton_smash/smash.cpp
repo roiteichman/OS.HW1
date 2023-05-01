505ec79bb66d471b68961cd96d24350215e067e7
teich <roi.teichman@campus.technion.ac.il>
2023-05-01 11:19:20 +0300

    one

--- skeleton_smash/smash.cpp
+++ skeleton_smash/smash.cpp
@@ -23,12 +23,12 @@ int main(int argc, char* argv[]) {
     SmallShell& smash = SmallShell::getInstance();
 
 
-    while(true) {
+//    while(true) {
         smash.printPrompt();
         std::string cmd_line;
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
-    }
+//    }
 
 
 
